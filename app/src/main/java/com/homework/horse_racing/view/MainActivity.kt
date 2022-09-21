b1e5b8c05d3fd49caac86e99249fb363acb761a4
Kingchenkong <wikeyno@gmail.com>
2022-09-21 15:26:59 +0800

    Add: viewmodel: ui, api(ExchangeRate),

--- app/src/main/java/com/homework/horse_racing/view/MainActivity.kt
+++ app/src/main/java/com/homework/horse_racing/view/MainActivity.kt
@@ -1,18 +1,10 @@
 package com.homework.horse_racing.view
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import androidx.lifecycle.ViewModel
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import com.google.gson.Gson
-import com.google.gson.JsonObject
-import com.homework.horse_racing.R
 import com.homework.horse_racing.databinding.ActivityMainBinding
-import com.homework.horse_racing.model.api.AppClientManager
-import com.homework.horse_racing.model.api.response_body.exchange_rate.ExchangeRateRespBody
-import com.homework.horse_racing.model.api.response_body.exchange_rate.USDTWD
 import com.homework.horse_racing.view_model.ExchangeApiViewModel
 import com.homework.horse_racing.view_model.ui.MainActivityViewModel
 
@@ -23,43 +15,64 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private lateinit var mainActivityViewModel: MainActivityViewModel
-    private lateinit var exchangeApiViewModel: ExchangeApiViewModel
+    private lateinit var uiVm: MainActivityViewModel
+    private lateinit var apiVm: ExchangeApiViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.d(TAG, "onCreate: ")
 
-        mainActivityViewModel = ViewModelProvider(this)[MainActivityViewModel::class.java]
-        exchangeApiViewModel = ViewModelProvider(this)[ExchangeApiViewModel::class.java]
+        uiVm = ViewModelProvider(this)[MainActivityViewModel::class.java]
+        apiVm = ViewModelProvider(this)[ExchangeApiViewModel::class.java]
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.lifecycleOwner = this
-        binding.uiVm = mainActivityViewModel
-        binding.apiVm = exchangeApiViewModel
+        binding.uiVm = uiVm
+        binding.apiVm = apiVm
 
+        initUiObserver()
+        initApiObserver()
+
+        uiAction()
     }
 
     override fun onStart() {
         super.onStart()
+        Log.d(TAG, "onStart: ")
 
-        queryExchangeRate() // TODO: use liveData
+        initView()
+
+        uiVm.updateExchangeRate(-1.0)
+        apiVm.queryExchangeRate()
     }
 
-    private fun queryExchangeRate() {
-        lifecycleScope.launchWhenStarted {
-            val response = AppClientManager.queryExchangeRate()
-            val gson = Gson()
-            when {
-                response.isSuccess -> {
-                    val json = response.getOrNull() ?: JsonObject()
-                    val body = gson.fromJson(json, ExchangeRateRespBody::class.java)
-                    val USDToTWD: USDTWD = body.USDTWD
-                    Log.d(TAG, "queryExchangeRate: USD to TWD: $USDToTWD")
-                }
-                else -> {}
+    private fun initView() {
+        uiVm.updateExchangeRate(0.0)
+
+    }
+
+    private fun initUiObserver() {
+
+    }
+
+    private fun initApiObserver() {
+        apiVm.exchangeApiLiveData.observe(this) {
+            Log.d(TAG, "initObserver: exchangeRateLiveData: $it")
+            uiVm.updateExchangeRate(it.Exrate)
+            uiVm.updateTime(it.UTC)
+        }
+
+    }
+
+    private fun uiAction() {
+        binding.apply {
+            btnApi.setOnClickListener {
+                Log.d(TAG, "uiAction: btnApi: onClick: ")
+                apiVm!!.queryExchangeRate()
             }
         }
     }
+
 
 }
--- app/src/main/java/com/homework/horse_racing/view_model/ui/MainActivityViewModel.kt
+++ app/src/main/java/com/homework/horse_racing/view_model/ui/MainActivityViewModel.kt
@@ -1,16 +1,53 @@
 package com.homework.horse_racing.view_model.ui
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.homework.horse_racing.model.api.response_body.exchange_rate.USDTWD
+import java.text.SimpleDateFormat
+import java.util.*
 
 class MainActivityViewModel : ViewModel() {
     companion object {
         private val TAG: String = MainActivityViewModel::class.java.simpleName
     }
 
+    private val _exchangeRateText: MutableLiveData<String> = MutableLiveData<String>()
+    var exchangeRateText: LiveData<String> = _exchangeRateText
+
+    private val _timeText: MutableLiveData<String> = MutableLiveData<String>()
+    var timeText: LiveData<String> = _timeText
+
     override fun onCleared() {
         super.onCleared()
         Log.d(TAG, "onCleared: ")
     }
 
+    fun updateExchangeRate(rate: Double) {
+        if (rate < 0) {
+            _exchangeRateText.postValue("loading...")
+        } else {
+            _exchangeRateText.postValue(exchangeRateToText(rate))
+        }
+    }
+
+    fun updateTime(timeString: String) {
+        _timeText.postValue(parseUTCToTW(timeString))
+    }
+
+    private fun exchangeRateToText(exchangeRate: Double): String {
+        return String.format("%.2f", exchangeRate)
+    }
+
+    private fun parseUTCToTW(utcTimeString: String): String {
+        val pattern = "yyyy-MM-dd HH:mm:ss"
+        val utcDF = SimpleDateFormat(pattern, Locale.UK)
+        utcDF.timeZone = TimeZone.getTimeZone("UTC")
+        val twDF = SimpleDateFormat(pattern, Locale.TAIWAN)
+        twDF.timeZone = TimeZone.getDefault()
+        val date: Date = utcDF.parse(utcTimeString) ?: Date()
+
+        return twDF.format(date)
+    }
 }
